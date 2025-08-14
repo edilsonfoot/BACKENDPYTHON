ec7b8f806c2bbcc6ca2df85861f09f8c304c40cf
edilsonfoot <edilsonpsilva123@gmail.com>
2025-08-14 19:59:53 +0000

    atividade aula04

---
+++ AULA BACK-END 04.py
@@ -0,0 +1,11 @@
+#### promoção supermercado
+for fregues in range(100):
+    fregues+-1
+    print(fregues)
+    if fregues == 100:
+        print("vc ganhou!!!!")
+else:
+    print("não sei o que isso faz")        
+
+
+    
