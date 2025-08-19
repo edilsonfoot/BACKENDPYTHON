493d2e470f6b77484798df82fd8b19459fac5a3b
Edilson Paulo <edilsonpaulo@example.com>
2025-08-19 06:28:30 -0300

    criando uma calculadora

--- exercicio_aula04.py
+++ exercicio_aula04.py
@@ -1,8 +1,67 @@
-numero = 123
-print(123)
+a = "123"
+a = int("123")
+a = float("123")
 
-numero = 123
-print("123")
+texto = "Python é incrível!"   
+quantidade = len("Python é incrível")
+print(quantidade)
+
+texto = "Python é incrível!"
+texto_maiusculo = texto.upper()
+print(texto_maiusculo)
+
+texto = "Pithon é incrível!"
+texto_novo = texto.replace("incrível", "poderoso")
+print(texto_novo)
+
+lista = [10, 20, 30, 40, 50]
+print(lista[2])
+      
+
+lista = [10, 20, 30, 40, 50, 60]
+print(lista)
+
+lista = [10, 30, 40, 50, 60]
+print(lista)
+
+"nome": "Maria"
+"idade": 22
+"curso": "Engenharia"
+"notas": [8.5, 7.0, 9.2]
+print(type(notas))
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
 
 
 
