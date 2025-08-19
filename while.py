5f47c14b02abe6ef88ff480bc03d4ed5c571eafa
edilsonfoot <edilsonpsilva123@gmail.com>
2025-08-19 17:59:15 +0000

    funções do while

---
+++ while.py
@@ -0,0 +1,18 @@
+for i in range(-1, -5, -1):
+                                      
+count = 0                                        #FOR (PASSA POR TODOS OS ELEMENTOS)
+while True:
+    print("dentro do while")
+    count+=1
+    if count == 4:
+
+count = 0
+while count < 4:
+    count +-1
+
+    count = 0
+    while count <=10:
+        count +=1
+    else:    
+
+                                            
