226c8851a5b9e09380543eecc04f21d28c579df2
edilsonfoot <edilsonpsilva123@gmail.com>
2025-08-26 19:26:27 +0000

    criando tupla

---
+++ conjuntos.py
@@ -0,0 +1,25 @@
+#Criando um conjunto
+
+#livros =  {"Duna", "Hannibal", "O pistoleiro", "Duna"}
+#print(livros)
+
+
+#método Add
+#livros = {"Duna", "Hannibal", "O pistoleiro", "Duna"}      # adiciona o um ou mais livros
+#livros.add("O conde de Monte Cristo")
+#print(livros)
+
+#método.update
+
+# método .union() ou |    #remova
+livros = {"Duna", "Hannibal", "O pistoleiro"}
+livros_terror.union = {"cujo", "It"}
+print(livros)
+
+#método.remove
+#livros = {"Duna", "Hannibal", "O pistoleiro", "Duna"}       # remove quantos elementos tiverem com o mesmo nome
+#livros.remove("Duna")
+#print(livros)
+
+
+
