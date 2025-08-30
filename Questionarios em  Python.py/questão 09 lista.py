1bef61cd1244637e6bd07714f89f993360e81596
Edilson Paulo <edilsonpaulo@example.com>
2025-08-30 15:07:16 -0300

    questao 09

---
+++ Questionarios em  Python.py/questão 09 lista.py
@@ -0,0 +1,14 @@
+'''
+Crie uma lista valores = [10, 20, 30, 40]. Use um laço for para calcular manualmente a soma de todos os valores.
+'''
+
+#lista principal
+valores = [10, 20, 30, 40]
+
+#variável
+soma = 0
+
+#calculando manualmaente e exibindo a soma total dos valores da lista
+for v in valores:
+    soma += v
+print(soma)
