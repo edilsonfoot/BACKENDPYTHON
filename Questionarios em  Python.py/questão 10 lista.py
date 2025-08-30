6babc3ad85823fe23b3ec60d3fa5c5f995d9cb92
Edilson Paulo <edilsonpaulo@example.com>
2025-08-30 17:27:52 -0300

    questao 10

--- Questionarios em  Python.py/questão 10 lista.py
+++ Questionarios em  Python.py/questão 10 lista.py
@@ -3,21 +3,22 @@ Use input para receber 3 notas de dois alunos. As notas de cada aluno precisam s
 notas = [[7, 8, 9], [6, 5, 7]]. No fim, imprima a média de cada aluno.
 '''
 
-
 #coletando notas de 2  alunos
+
 for i in range(2):
     aluno_notas = []
-    print(f"\ndigite as três notas do aluno {i + 1}:")    
+    print(f"\nDigite as 3 notas do aluno {i + 1}:")    
 
 
     for j in range(3):
-        n = float(input("Nota {j + 1}: "))                
-        aluno_notas.append(nota) 
+        n = float(input("Nota {j + 1}:"))                
+        aluno_notas.append(n) 
 
 
-notas.append(aluno_notas)
+n.append(aluno_notas)
 
-#calculando a média de cada aluno e exibindo-as
-for i, aluno in enumerate(notas, start=1):
+#calculando a média de cada aluno e exibindo
+for i, aluno in enumerate(n, start=1):
     media = sum(aluno) / len(aluno)
     print(f"A media do aluno {i} é: {media:.2f}")
+
