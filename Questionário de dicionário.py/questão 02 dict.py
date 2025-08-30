039da792ba138f5008b87a93a6d55f16b4ba69e1
Edilson Paulo <edilsonpaulo@example.com>
2025-08-30 18:05:38 -0300

    questao 02

---
+++ Questionário de dicionário.py/questão 02 dict.py
@@ -0,0 +1,12 @@
+'''
+Acessando valores
+
+Dado o dicionário:
+
+produto = {"nome": "Caneta", "preço": 2.5, "estoque": 100}
+Imprima o nome do produto e a quantidade em estoque.
+
+'''
+
+produto = {"nome": "Caneta", "preço": 2.5, "estoque": 100}
+print(produto["nome"], produto["estoque"])
