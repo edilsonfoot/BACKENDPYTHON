f510d33ddba274e47bcab8b7797fbfd15f8ff231
Edilson Paulo <edilsonpaulo@example.com>
2025-08-30 14:55:46 -0300

    questão 08

--- Questionarios em  Python.py/questão 07 lista.py
+++ Questionarios em  Python.py/questão 07 lista.py
@@ -2,11 +2,9 @@
 Percorra a lista livros e exiba cada livro com a frase: "O livro <nome do livro> é interessante"
 '''
 
-#método def
-
 livros = ["romance", "religião", "Duna", "politica", "sociologia", "matematica"]
 
-#percorrendo lista
+#Usando a estrutura de repetição "for" para percorrer a lista
 
 for livro in livros:
         print(f"O livro '{livro}' é interessante!")
---
+++ Questionarios em  Python.py/questão 08 lista.py
@@ -0,0 +1,10 @@
+'''
+Dada a lista idades = [12, 18, 25, 14, 30], use um laço para exibir somente as idades maiores ou iguais a 18.
+'''
+
+idades = [12, 18, 25, 14, 30]
+
+#Usando a estrutura de repetição "for" e a condicional "if"
+for i in idades:
+    if i >= 18:
+        print(i)
