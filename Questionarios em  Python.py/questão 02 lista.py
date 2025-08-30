c19b62c3304f5b4fbca62af1445f8597c1f9e09a
Edilson Paulo <edilsonpaulo@example.com>
2025-08-30 13:47:15 -0300

    questão 02

---
+++ Questionarios em  Python.py/questão 02 lista.py
@@ -0,0 +1,9 @@
+'''
+Usando a lista livros, exiba apenas o primeiro e o último elemento.
+'''
+#Usando o método .remove()
+livros = ["romance", "religião", "politica"]   
+
+#imprimindo apenas "o primeiro e ultimo"
+livros.remove("religião")
+print(livros)
