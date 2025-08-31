d55de0365476b6af9374a16e99a2c1b92c1a00c7
Edilson Paulo <edilsonpaulo@example.com>
2025-08-31 13:52:36 -0300

    revisando aula de match

--- match.py
+++ match.py
@@ -1,6 +1,6 @@
-match = expressão
+#match = expressão
 
-
+'''
 num = input("imforme um numero de 1 a 5: ")
 match num:
     case "1" 
@@ -12,13 +12,57 @@ match num:
     case "4" 
         print(f'você digitou {num}")
     case "0"  
-    
+'''  
             
-        
-              
-              
-              
+
+
+''''
+#dias da semana 
+
+def dia_semana(dia):
+    if dia == 1:
+        return " domingo"
+
+    elif dia == 2:
+        return "segunda"
+    elif dia == 3:
+        return "terça-feira"
+    elif dia == 4:
+        return "quarta-feira"
+    elif dia == 5:
+        return "quinta-feira"    
+    elif dia == 6:
+        return "sexta-feira"
+    elif dia == 7:
+        return "sábado"    
+    else:
+        return "valor {} invalido".format(dia)          
+       dia_semana() > if dia == 1
+''''''
+
+
+def dia_semana(dia):
+    match dia:
+           case 1:
+               return " domingo"
+           case 2:
+               return "segunda"
+           case 3:
+               return "terça-feira"
+           case 4:
+               return "quarta-feira"
+           case 5:
+               return "quinta-feira"    
+           case  6:
+               return "sexta-feira"
+           case 7:
+               return "sábado"  
+           case _:
+              return "valor {} inválido".format(dia)
+    
+
+print(dia_semana(1))
+print(dia_semana(5))
+
        
 
-      
-              
