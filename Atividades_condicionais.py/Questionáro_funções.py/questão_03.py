89b9f4b82b9f39652276a5ea0899e8211511ae59
Edilson Paulo <edilsonpaulo@example.com>
2025-09-03 12:52:17 -0300

    questão 03

---
+++ Atividades_condicionais.py/Questionáro_funções.py/questão_03.py
@@ -0,0 +1,14 @@
+'''
+Crie uma função chamada soma que receba dois números e retorne a soma deles. Depois, use a função para somar 10 + 20
+'''
+
+num1 = 10
+num2 = 20
+
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é: {soma}")
+
+
+
+
+
