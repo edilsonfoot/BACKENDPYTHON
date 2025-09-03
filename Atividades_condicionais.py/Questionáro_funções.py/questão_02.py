1131366538a4a3ee4e9bd37726ea95f54cf8e4cb
Edilson Paulo <edilsonpaulo@example.com>
2025-09-03 12:15:56 -0300

    questão 02

---
+++ Atividades_condicionais.py/Questionáro_funções.py/questão_02.py
@@ -0,0 +1,14 @@
+'''
+Crie uma função chamada dobro que recebe um número como parâmetro e retorna o dobro desse número. Teste chamando a função com diferentes valores.
+'''
+
+
+numero = float(input("7: "))
+               
+bobro = numero *2
+dobro = 7 *2
+print(f"O dobro de {numero} é {dobro}.")               
+    
+
+
+
