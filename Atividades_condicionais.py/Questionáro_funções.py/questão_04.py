2ca66302c10c5dcacaae654cb291582ca36a7c8a
Edilson Paulo <edilsonpaulo@example.com>
2025-09-04 09:22:03 -0300

    questão o4

--- Atividades_condicionais.py/Questionáro_funções.py/questão_04.py
+++ Atividades_condicionais.py/Questionáro_funções.py/questão_04.py
@@ -5,9 +5,9 @@ Crie uma função chamada mensagem que receba um nome como parâmetro e exiba a 
 
           Caso o nome não seja informado, mostre "Olá, visitante!".
 '''
-
-saudação = "Olá, [nome]!"
-def Olá(nome = "nome" ):
+nome = "Edilson"
+saudação = "Olá, [Edilson]!"
+def Olá(nome="nome"):
     if nome:
         return f"Olá, {nome}"
     else:
@@ -15,3 +15,17 @@ def Olá(nome = "nome" ):
 
 print(saudação("nome"))
 print(saudação())
+
+
+
+
+
+
+'''
+def mensagem(nome):
+    print(f"Olá, {nome}! Seja bem-vindo(a)!")
+
+def Olá(nome):
+    print(f"Olá, {mome}! seja ben-vindo(a)!")
+'''
+
--- Atividades_condicionais.py/Questionáro_funções.py/questão_05.py
+++ Atividades_condicionais.py/Questionáro_funções.py/questão_05.py
@@ -4,20 +4,20 @@ Crie uma função chamada operacoes que receba dois números e retorne a soma, a
 
 '''
 #função de adição retornando a soma
-def soma_num(num1,num2):
+def oper_soma_num(num1,num2):
     return (num1 + num2)
 
-resultado = soma_num(10,20)
+resultado = oper_soma_num(10,20)
 print(resultado)
 '''
 
 #função de subtração
 
 '''
-def subtração_num(num1,num2):
+def oper_subtração_num(num1,num2):
     return (num1 - num2)
 
-resultado = subtração_num(20,10)
+resultado = oper_subtração_num(20,10)
 print(resultado)
 '''
 
