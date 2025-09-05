c433a7e30a44134de24721af29b1f0fe7570462b
Edilson Paulo <edilsonpaulo@example.com>
2025-09-05 20:03:41 -0300

    questão 02

--- Atividades_condicionais.py/Questionáro_funções.py/questão_02.py
+++ Atividades_condicionais.py/Questionáro_funções.py/questão_02.py
@@ -3,12 +3,24 @@ Crie uma função chamada dobro que recebe um número como parâmetro e retorna 
 '''
 
 
-numero = float(input("7: "))
-               
-bobro = numero *2
-dobro = 7 *2
-print(f"O dobro de {numero} é {dobro}.")               
-    
+
+def função_dobro(x):  
+    return x * 2
+'''
+numero = 10
+resultado = função_dobro(10)
+print(f"O dobro de {10} é {resultado}.")
+'''
+'''
+numero = 50
+resultado = função_dobro(50)
+print(f"O dobro de {50} é {resultado}.")
+'''
+
+
+numero = 100
+resultado = função_dobro(100)
+print(f"O dobro de {100} é {resultado}.")    
 
 
 
